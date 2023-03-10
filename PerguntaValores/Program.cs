25eef7bc6078888f9b690cf03f3d8a8f1787bfe8
Carolina Bastos <carol1999b@gmail.com>
2023-03-10 10:54:56 +0000

    Add PerguntaValores

---
+++ PerguntaValores/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PerguntaValores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string heightStr, radiusStr;
+            float height, radius, volume, sArea;
+
+            Console.WriteLine("Altura:");
+            heightStr = Console.ReadLine();
+            Console.WriteLine("Raio:");
+            radiusStr = Console.ReadLine();
+
+            height = float.Parse(heightStr);
+            radius = float.Parse(radiusStr);
+
+            volume = (float)3.1415926 * (radius * radius) * height;
+            sArea = (2 * (float)3.1415926) * radius * (radius + height);
+
+            Console.WriteLine($"Volume = {volume}\nSurface Area = {sArea}");
+        }
+    }
+}
