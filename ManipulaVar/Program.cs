930a088aea622e444ea2a8fd52cc401d43570dbe
Carolina Bastos <carol1999b@gmail.com>
2023-03-10 10:31:17 +0000

    Add operator --

--- ManipulaVar/Program.cs
+++ ManipulaVar/Program.cs
@@ -7,13 +7,22 @@ namespace ManipulaVar
         static void Main(string[] args)
         {
             int x = Convert.ToInt32(args[0]);
+            int y = x;
 
             int a = 2, b;
             b = a + x++;
 
             Console.WriteLine(b);
-            Console.WriteLine(x);
-            
+            Console.WriteLine($"valor de x = {x}");
+
+            int c = 3, d;
+            d = c + --y;
+
+            Console.WriteLine(d);
+            Console.WriteLine($"valor de x = {y}");
+
+
+
         }
     }
 }
